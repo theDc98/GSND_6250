1aeac7a41a02ac6fef72fb1d3af93c06c9e7b066
D.c <63380156+ucscDc98@users.noreply.github.com>
2022-09-21 21:43:52 -0400

    - Add timer & End
    - Game Over UI and Timer UI

--- Project_1/Proj_1/Assets/Scripts/Player.cs
+++ Project_1/Proj_1/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,15 +9,24 @@ public class Player : MonoBehaviour
     public int acc = 5;
     Vector3 facing_direction = new Vector3(0.0f, 0.0f, 0.0f);
 
+    private int cubes = 0;
+    private float timer = 0;
+
+    public Text text;
+    public GameObject ui;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Current Speed is: " + speed);
+        timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
         var characterController = this.GetComponent<CharacterController>();
         var direction = new Vector3(
             Input.GetAxis("Horizontal"),
@@ -31,11 +41,19 @@ public class Player : MonoBehaviour
         var target = transform.position + facing_direction;
         transform.LookAt(target);
 
+        if(cubes >= 4)
+        {  
+            Time.timeScale = 0;
+            text.text = timer.ToString();
+            ui.SetActive(true);
+        }
+
     }
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("Cube"))
         {
+            cubes++;
             Debug.Log("SPEED UP! Current Speed is: " + speed);
             speed += acc;
             Destroy(collider.gameObject);
